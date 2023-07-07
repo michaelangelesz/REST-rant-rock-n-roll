d2543184544a8152e103017914bea5b37a3d38c0
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-07-07 10:42:06 -0400

    add 404 page route

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-// 1. Require express at the top of the file
-
-// 2. Initialize the app variable
-
-// 3. Create the home page route
-//    a. Call app.get()
-//    b. Set ‘/‘ as the path (first arg)
-//    c. Write callback function with req, res
-//    d. Call res.send(‘hello world’) 
-
-// 4. Call app.listen(8080) to keep server open
-
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
@@ -17,4 +6,8 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(8080)
+app.get('*', (req, res) => {
+    res.status(404).send('404 Not Found')
+})
+
+app.listen(process.env.PORT)
