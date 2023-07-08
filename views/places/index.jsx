abe1cc3507696b99745a829488eec82250c5c9ab
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-07-08 18:58:05 -0400

    fix key props

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,14 +2,14 @@ const React = require('react')
 const Def = require('../default')
 
 function index (data) {
-    let placesFormatted = data.places.map((place) => {
-      return (
-        <div>
-          <h2>{place.name}</h2>
-          <img src={place.pic} alt={place.name}/>
-        </div>
-      )
-    })
+    let placesFormatted = data.places.map((place, index) => {
+        return (
+          <div key={index}>
+            <h2>{place.name}</h2>
+            <img src={place.pic} alt={place.name}/>
+          </div>
+        )
+      })
     return (
       <Def>
           <main>
