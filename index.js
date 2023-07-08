267657bcf365d85095650ce10dfe216fecbff6e8
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-07-08 18:22:41 -0400

    Adding 404 page view

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('404 Not Found')
-})
-
+    res.render('error404')
+  })
+  
 app.listen(process.env.PORT)
